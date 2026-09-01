4aeaaae3fd88d4ff658b2e33d94c522ecfe2110a
BryceBrooks <bbrooks869@boyslatin.net>
2026-09-01 14:24:34 +0000

    datatype notes

---
+++ Unit 1/datatypes.py
@@ -0,0 +1,33 @@
+#string - characters that are wrapped within quotes.
+#characters can be letters, numbers, and symbols.
+#you can use both single and double quotation
+#marks for strings
+
+"Bryce Brooks'
+
+'123 Main street'
+
+"this costs $100 Dollars"
+
+#the hastag is a marker that makes anything written
+#behind it invisible to the computer.
+
+#integers - datatypes that are just whole numbers, or numbers
+#without a decimal point. we dont have to add any special symbols
+
+10
+20
+200000
+
+# floats - number with a decimal.
+
+20.99
+3.339
+0.23
+
+#boolean - a data type that can only be either true or false
+# we do not use qutotation marks for boolean values. the computer
+# understand these words as something unique
+
+False
+True 
