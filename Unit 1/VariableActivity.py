9dfd6da293877f0a08d96cd7d4623846a466f9eb
BryceBrooks <bbrooks869@boyslatin.net>
2026-09-03 14:20:14 +0000

    VariableNaming

---
+++ Unit 1/VariableActivity.py
@@ -0,0 +1,18 @@
+# VARIABLES - a container that stores data. You can
+# name your container almost anything you want, so long
+# as it is descriptinve.
+
+# Variable Naming Rules
+# 1. A variable MUST start with a letter and cannot start with a number
+# 2. A variable can be a mixed of letters and numbers BUT not symbols
+# 3. A variale CANNOT be a Python keyword
+
+appleCount = 42
+numberofStudents = 27
+YesinsuranceCost = $100,000
+BrozeMembership = 27
+SilverMembership = 15
+GoldMembership = 7
+Drink1 = $17
+Drink2 = $15
+Drink3 = $10
