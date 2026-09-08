cbcaadf49a7cea2fd7d090aeaa59c851853ab1f2
BryceBrooks <bbrooks869@boyslatin.net>
2026-09-08 14:04:26 +0000

    AI work

--- Unit 1/VariableActivity.py
+++ Unit 1/VariableActivity.py
@@ -16,3 +16,19 @@ GoldMembership = 7
 Drink1 = $17
 Drink2 = $15
 Drink3 = $10
+
+# Variable Naming Conventions
+# we use naming conventions because we CANNOT have specs in
+# variable names
+
+# Camel Case - when creating variables names with multiple words,
+# every new word begins with a capital letter
+# the reason why its called camel case its because every new word
+# represents a hump.
+usernameForNewInstagramAccounts = 0
+passwordRecoveryTiktokAccount = 0
+
+# Snake Case - when creating a variable using snake case
+# we use a underscore symbol to space out each new word
+# the reason why its called snake case its because the underscore
+# represents a snake on the ground  new_word_
