c89531be5fdd9fb312fde7664d79488dab3a7bf9
BryceBrooks <bbrooks869@boyslatin.net>
2026-09-09 14:35:44 +0000

    operator notes

---
+++ Unit 1/operators.py
@@ -0,0 +1,30 @@
+# Assignment Operators - Exclusively used to
+# asign values to variables.
+# think key / value pairings
+
+# We use a single equal sign to represent the assignment
+# operator
+name = "Bryce" 
+grade = 10
+school = True
+sneakersInShoe = 2
+# Arithmetic Operators - Used on numerical
+# data types to perform calculations.
+# intergers (whole numbers) and floats (decimal numbers) 
+
+# print is a functions= that lets us show code
+# in the terminal
+print(3 + 3) # addition operator
+print(3.9 - 3) # subtraction operator
+print (12 / 3) # division operator
+print (3 * 4) # multiplication operator
+
+# Comparison Operators - Set of symbols used
+# to assess if data is the same or different and
+# how they differ
+
+print(10 > 1) # greater than operator
+print(10 < 2) # less than operator
+
+# 2 equal signs compare if something is the same
+print("bryce == Bryce") # same as (false)
