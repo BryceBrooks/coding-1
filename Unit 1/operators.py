cc5d90cca9ba40b3e3501187ad4b15763bb4fb00
BryceBrooks <bbrooks869@boyslatin.net>
2026-09-10 14:33:33 +0000

    Operator update

--- Unit 1/operators.py
+++ Unit 1/operators.py
@@ -28,3 +28,30 @@ print(10 < 2) # less than operator
 
 # 2 equal signs compare if something is the same
 print("bryce == Bryce") # same as (false)
+print("2" == 2) # same as (false)
+print (2.0 == 2) #same as (true)
+
+# not equal is written with !=
+# this is to check and filter for values that are not
+# the same
+print(200 != 100) # True - these are not the same
+print(300 != 300) # False - these are not the same
+
+# logical operators - compares 2 conditions to check if
+# they are true or false
+# conditions = other operators
+# instead of symbols, we represent these with words:
+# and, or, not
+
+# AND - checks if 2 conditions are true. if yes, the final
+# result is ture
+print(3 > 1 and 100 > 50) # this would come out to be true
+
+# OR - checks if only 1 condition is true. if yes,
+# the final result will be true
+print(3 < 1 or 100 > 50)
+
+# NOT - the "opposite day" operator. it will reverse the
+# result of the logical operators
+print(not(3 > 1 and 100 > 50))
+#this would come out to be false
